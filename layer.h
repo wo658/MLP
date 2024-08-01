99a7fa6972ba3df1195bd4baedfe4d18b4ac471d
wo658 <68763647+wo658@users.noreply.github.com>
2024-08-01 17:56:27 +0900

    Add files via upload
    asdasd

--- layer.h
+++ layer.h
@@ -1,5 +1,17 @@
 #pragma once
 
+enum activefunc {
+	SIGMOID,
+	LINEAR
+};
+
+double sigmoid_derivative(double value) {
+	return value * (1.0 - value);
+}
+double linear_derivative(double vlaue) {
+	return 1;
+}
+double learning_rate = 0.0005;
 class layer {
 
 
@@ -11,7 +23,7 @@ public:
 
 		values.resize(num);
 		active_values.resize(num);
-		diff.resize(num);
+		diff.resize(pre_num);
 	};
 	vector<vector<double>> weight;
 	vector<double> values;
@@ -20,8 +32,11 @@ public:
 	int node_num;
 	int node_pre_num;
 	double bias;
-
-
+	double sigmoid(double x) {
+		return 1.0 / (1.0 + exp(-x));
+	}
+	virtual void backpropagation() {};
+	
 	void init() {
 		for (int i = 0; i < node_pre_num; i++) {
 			for (int j = 0; j < node_num; j++) {
@@ -38,13 +53,45 @@ public:
 		bias = 3;
 
 	}
+	void feedforward(vector<double>left_active_values,activefunc active_func_name) {
+		for (int j = 0; j < node_num; j++) {
+			values[j] = bias;
+			for (int i = 0; i < node_pre_num; i++) 
+				values[j] += left_active_values[i] * weight[i][j];
+
+			if (active_func_name == SIGMOID)
+				active_values[j] = sigmoid(values[j]);
+			else if (active_func_name == LINEAR)
+				active_values[j] = values[j];
+		}
+}
 
 };
+
+
+
 // 행이 이전 노드 열이 다음 노드 
 class output_layer : public layer {
 public:
 	output_layer(int pre_num, int num) : layer(pre_num, num) {
 	};
+
+	void backpropagation(double answer, double predict, vector<double>left_active_values,activefunc active_func_name) {
+		double delta;
+		if (active_func_name == SIGMOID)
+			delta = (predict - answer) * sigmoid_derivative(predict);
+		else if (active_func_name == LINEAR)
+			delta = (predict - answer) * linear_derivative(predict);
+
+		for (int j = 0; j < node_pre_num; j++) {
+			weight[j][0] -= learning_rate * delta * left_active_values[j];
+			diff[j] = delta * weight[j][0];
+		}
+		bias -= learning_rate * delta;
+
+		
+	};
+
 };
 class hidden_layer : public layer {
 
@@ -52,9 +99,27 @@ public:
 	hidden_layer(int pre_num, int num) : layer(pre_num, num) {
 	};
 
+	void backpropagation(vector<double> right_diff,vector<double>left_active_values,activefunc active_func_name) {
+		double delta;
+		for (int k = 0; k < node_num; k++) {
+			if (active_func_name == SIGMOID)
+				delta = right_diff[k] * sigmoid_derivative(active_values[k]);
+			else if (active_func_name == LINEAR)
+				delta = right_diff[k] * linear_derivative(active_values[k]);
+			else
+				cout << "WWWWWW";
+			for (int j = 0; j < node_pre_num; j++) {
+				weight[j][k] -= learning_rate * delta * left_active_values[j];
+				diff[j] += delta * weight[j][k];
+			}
+			bias -= learning_rate * delta;
+		}
+	};
 
 };
 
 
 // 1 . wegiht 지정
 // 2 . #pragma once
+
+// back 기준 이전레이어의 Diff값 
--- main.cpp
+++ main.cpp
@@ -1,141 +1,177 @@
 #include "MLP_Functions.h"
 #include "Layer.h"
 
+#include <windows.h>
 double sigmoid(double x) {
-    return 1.0 / (1.0 + exp(-x));
+	return 1.0 / (1.0 + exp(-x));
 }
 int main() {
-    //Regression 
-    //Data Load
-    string dataPath = "C:\\Users\\whdgn\\source\\repos\\Project1\\ProcessDifference_train.csv";
-    const char* NameofData = dataPath.c_str();
-    vector<vector<double>> train;
-    train = readFile(NameofData);
-
-    //x_train/Y_train Split
-    vector<vector<double>> x_train, y_train;
-    splitData(train, x_train, y_train);
-
-
-    // 입력 차원 54  출력 차원 1  , data 수 6999개
-    // x_train[0]~x_train[6998]
-    // 미완 -> 정규화  ,   backpropagation
-
-    // cout << x_train[0].size() << endl;
-    hidden_layer h_layer1(54, 32), h_layer2(32, 16);
-    output_layer o_layer(16, 1);
+	//Regression 
+	//Data Load
+	string dataPath = "C:\\Users\\ecmdev\\Desktop\\123\\MLP_Ex\\MLP_Ex\\ProcessDifference_train.csv";
+	string dataPath_test = "C:\\Users\\ecmdev\\Desktop\\123\\MLP_Ex\\MLP_Ex\\ProcessDifference_test.csv";
+	const char* NameofData = dataPath.c_str();
+	const char* testData = dataPath.c_str();
+	vector<vector<double>> train;
+	vector<vector<double>> test;
+	train = readFile(NameofData);
+	test = readFile(testData);
 
 
-    double error;
-    double learning_rate = 0.1;
-    double y_answer = 0, y_predict = 0;
+	//x_train/Y_train Split
+	vector<vector<double>> x_train, y_train;
+	vector<vector<double>> x_test, y_test;
+	splitData(train, x_train, y_train);
+	splitData(test, x_test, y_test);
+
+
+	// 입력 차원 54  출력 차원 1  , data 수 6999개
+	// x_train[0]~x_train[6998]
+	//정규화  ,   backpropagation
+
+	// cout << x_train[0].size() << endl;
+	hidden_layer h_layer1(54, 32), h_layer2(32, 16);
+	output_layer o_layer(16, 1);
+
+
+	double error;
+	
+	double y_answer = 0, y_predict = 0;
 
 
 
 
 
-    // 정규화 최대 최소 범위 구하기
-    double max = x_train[0][0], min = x_train[0][0];
-    for (int i = 0; i < x_train.size(); i++)
-        for (int j = 0; j < x_train[0].size(); j++) {
-            if (max < x_train[i][j])
-                max = x_train[i][j];
-            if (min > x_train[i][j])
-                min = x_train[i][j];
-        }
+	// 정규화 최대 최소 범위 구하기
+	double max = x_train[0][0], min = x_train[0][0];
+	for (int i = 0; i < x_train.size(); i++)
+		for (int j = 0; j < x_train[0].size(); j++) {
+			if (max < x_train[i][j])
+				max = x_train[i][j];
+			if (min > x_train[i][j])
+				min = x_train[i][j];
+		}
 
-    cout << max << " " << min << endl;
-    // 정규화 0 ~ 1 
-    for (int i = 0; i < x_train.size(); i++) {
-        for (int j = 0; j < x_train[0].size(); j++) {
-            x_train[i][j] = (x_train[i][j] - min) / (max - min);
-        }
-        y_train[i][0] = (y_train[i][0] - min) / (max - min);
-    }
+	//cout << max << " " << min << endl;
+	// 정규화 0 ~ 1 
+	for (int i = 0; i < x_train.size(); i++) {
+		for (int j = 0; j < x_train[0].size(); j++) {
+			x_train[i][j] = (x_train[i][j] - min) / (max - min);
+		}
 
-
-    h_layer1.init();
-    h_layer2.init();
-    o_layer.init();
-
-    /////////////////////////////////////////////////////////////////////////////
-
-    for (int epho = 0; epho < 100; epho++) {
-        for (int i = 0; i < x_train.size(); i++) {
-            y_answer = y_train[i][0];
-
-            // feedforward
-            for (int k = 0; k < h_layer1.node_num; k++) {
-                h_layer1.values[k] = h_layer1.bias;
-                for (int j = 0; j < x_train[0].size(); j++) {
-                    h_layer1.values[k] += x_train[i][j] * h_layer1.weight[j][k];
-                }
-                h_layer1.active_values[k] = sigmoid(h_layer1.values[k]);
-            }
-
-            for (int k = 0; k < h_layer2.node_num; k++) {
-                h_layer2.values[k] = h_layer2.bias;
-                for (int j = 0; j < h_layer1.node_num; j++) {
-                    h_layer2.values[k] += h_layer1.active_values[j] * h_layer2.weight[j][k];
-                }
-                h_layer2.active_values[k] = sigmoid(h_layer2.values[k]);
-            }
-
-            o_layer.values[0] = o_layer.bias;
-            for (int j = 0; j < h_layer2.node_num; j++) {
-                o_layer.values[0] += h_layer2.active_values[j] * o_layer.weight[j][0];
-            }
-            o_layer.active_values[0] = sigmoid(o_layer.values[0]);
-
-            y_predict = o_layer.active_values[0];
-            error = (y_predict - y_answer);
+		// 선택사항 1 y 정규화
+		// 1 . 회귀 문제의 경우 타겟값이 넓은 범위에 걸쳐 있을 때 정규화
+		y_train[i][0] = (y_train[i][0] - min) / (max - min);
+	}
 
 
-            // backpropagation
-            double delta = (y_predict - y_answer) * y_predict * (1 - y_predict);
-            for (int j = 0; j < h_layer2.node_num; j++) {
-                o_layer.weight[j][0] -= learning_rate * delta * h_layer2.active_values[j];
-                h_layer2.diff[j] = delta * o_layer.weight[j][0];
-            }
-            o_layer.bias -= learning_rate * delta;
+	h_layer1.init();
+	h_layer2.init();
+	o_layer.init();
 
-            for (int k = 0; k < h_layer2.node_num; k++) {
-                double delta2 = h_layer2.diff[k] * h_layer2.active_values[k] * (1 - h_layer2.active_values[k]);
-                for (int j = 0; j < h_layer1.node_num; j++) {
-                    h_layer2.weight[j][k] -= learning_rate * delta2 * h_layer1.active_values[j];
-                    h_layer1.diff[j] += delta2 * h_layer2.weight[j][k];
-                }
-                h_layer2.bias -= learning_rate * delta2;
-            }
+	////////////////////////////////	 train      ////////////////////////////////////////////
 
-            for (int k = 0; k < h_layer1.node_num; k++) {
-                double delta3 = h_layer1.diff[k] * h_layer1.active_values[k] * (1 - h_layer1.active_values[k]);
-                for (int j = 0; j < x_train[0].size(); j++) {
-                    h_layer1.weight[j][k] -= learning_rate * delta3 * x_train[i][j];
-                }
-                h_layer1.bias -= learning_rate * delta3;
-            }
+	for (int epho = 0; epho < 10; epho++) {
+		for (int i = 0; i < x_train.size(); i++) {
+			y_answer = y_train[i][0];
 
-            // value 초기화
-            fill(h_layer1.values.begin(), h_layer1.values.end(), 0.0);
-            fill(h_layer2.values.begin(), h_layer2.values.end(), 0.0);
-            o_layer.values[0] = 0.0;
+			// feedforward
+			h_layer1.feedforward(x_train[i], SIGMOID);
+			h_layer2.feedforward(h_layer1.active_values, SIGMOID);
+			o_layer.feedforward(h_layer2.active_values, LINEAR);
 
-            cout << "error :" << error << endl;
-        }
-        cout << "error :" << error << endl;
-    }
+			//y_predict = o_layer.active_values[0];
+			y_predict = o_layer.values[0];
+			error = (y_predict - y_answer) * (y_predict - y_answer);
+
+			// output 노드에 활성화 함수를 적용시킬것인지
+
+
+			// backpropagation
+			//double delta = (y_predict - y_answer) * y_predict * (1 - y_predict);
+
+			o_layer.backpropagation(y_answer, y_predict, h_layer2.active_values, LINEAR);
+			h_layer2.backpropagation(o_layer.diff, h_layer1.active_values, SIGMOID);
+			h_layer1.backpropagation(h_layer2.diff, x_train[i], SIGMOID);
+
+
+
+			/*
+
+			
+			double delta = (y_predict - y_answer) * 1;
+			for (int j = 0; j < h_layer2.node_num; j++) {
+				o_layer.weight[j][0] -= learning_rate * delta * h_layer2.active_values[j];
+				h_layer2.diff[j] = delta * o_layer.weight[j][0];
+			}
+			o_layer.bias -= learning_rate * delta;
 
 
 
 
 
 
-    system("pause");
+			for (int k = 0; k < h_layer2.node_num; k++) {
+				double delta2 = h_layer2.diff[k] * h_layer2.active_values[k] * (1 - h_layer2.active_values[k]);
+				for (int j = 0; j < h_layer1.node_num; j++) {
+					h_layer2.weight[j][k] -= learning_rate * delta2 * h_layer1.active_values[j];
+					h_layer1.diff[j] += delta2 * h_layer2.weight[j][k];
+				}
+				h_layer2.bias -= learning_rate * delta2;
+			}
+
+			for (int k = 0; k < h_layer1.node_num; k++) {
+				double delta3 = h_layer1.diff[k] * h_layer1.active_values[k] * (1 - h_layer1.active_values[k]);
+				for (int j = 0; j < x_train[0].size(); j++) {
+					h_layer1.weight[j][k] -= learning_rate * delta3 * x_train[i][j];
+				}
+				h_layer1.bias -= learning_rate * delta3;
+			}
+			*/
+
+			//if (i % 100 == 0) {
+			//	cout << "error :" << error << endl;
+			//	Sleep(100);
+			//}
+		}
+		cout << "error :" << error << endl;
+		Sleep(1000);
+	}
+
+	////////////////////////////////////////////////	Test	 ///////////////////////////////////////
 
 
-    return 0;
+	// 정규화 0 ~ 1 
+	for (int i = 0; i < x_test.size(); i++) {
+		for (int j = 0; j < x_test[0].size(); j++) {
+			x_test[i][j] = (x_test[i][j] - min) / (max - min);
+		}
+
+		// 1 . 회귀 문제의 경우 타겟값이 넓은 범위에 걸쳐 있을 때 정규화
+		y_test[i][0] = (y_test[i][0] - min) / (max - min);
+	}
+
+
+	for (int i = 0; i < x_test.size(); i++) {
+		// value 초기화
+		fill(h_layer1.values.begin(), h_layer1.values.end(), 0.0);
+		fill(h_layer2.values.begin(), h_layer2.values.end(), 0.0);
+		o_layer.values[0] = 0.0;
+
+		y_answer = y_test[i][0];
+		h_layer1.feedforward(x_test[i], SIGMOID);
+		h_layer2.feedforward(h_layer1.active_values, SIGMOID);
+		o_layer.feedforward(h_layer2.active_values, LINEAR);
+
+		//y_predict = o_layer.active_values[0];
+		y_predict = o_layer.values[0];
+		error = (y_predict - y_answer) * (y_predict - y_answer);
+		cout << "error : " << error << endl;
+	}
+
+
+	system("pause");
+
+
+	return 0;
 }
-
-
-
